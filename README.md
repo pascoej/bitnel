7f9c91b338a02dff0564330fc9368acbb649f997
Andrew Tian <andrew@tian.io>
2013-12-26 04:22:58 -0600

    Setup initial db migration

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # bitnel-api
 
 Here we go again...
+
+## Database migrations
+
+Some package named [goose](https://bitbucket.org/liamstask/goose) is used for database migrations.
+
+Make sure to copy `db/dbconf.yml.example` to `db/dbconf.yml` and edit as nessecary.
---
+++ db/migrations/20131222225548_AddInitialSchema.sql
@@ -0,0 +1,25 @@
+-- +goose Up
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE users (
+    uuid uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email text NOT NULL,
+    password_hash text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE orders (
+    uuid uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    size bigint NOT NULL,
+    initial_size bigint NOT NULL,
+    price bigint,
+    side smallint NOT NULL,
+    status smallint NOT NULL,
+    type smallint NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE users;
+DROP TABLE orders;
+DROP EXTENSION "uuid-ossp";
