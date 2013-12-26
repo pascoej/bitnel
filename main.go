ef81dcbfe8a9ba0a636346193a32b43e4c71dd96
Andrew Tian <andrew@tian.io>
2013-12-26 04:58:51 -0600

    Connect to db

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 var config map[string]string
+var db *sql.DB
 
 func main() {
 	var err error
 	config, err = loadConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// driver does not check if there's an actual connection made
+	// do something about this later
+	db, err = sql.Open("postgres", config["database"])
 	if err != nil {
 		log.Fatal(err)
 	}
