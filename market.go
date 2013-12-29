1f9bb232e6470c3582e31ae95e3eb89628b9aba9
Andrew Tian <andrew@tian.io>
2013-12-29 03:04:42 -0600

    Manually specify currency pair for market

--- market.go
+++ market.go
@@ -4,8 +4,7 @@ type market struct {
 	uuid          string
 	baseCurrency  currency
 	quoteCurrency currency
-}
 
-func (m *market) currencyPair() string {
-	return m.baseCurrency.String() + m.quoteCurrency.String()
+	// need to specify for url routing
+	currencyPair string
 }
